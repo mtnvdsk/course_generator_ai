f733b52d34a13499bfaa0b7b8a31e8bb5ad44307
mtnvdsk <mtnvdsk@gmail.com>
2025-09-30 18:20:48 +0530

    db update

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,22 +1,19 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectToDatabase = async () => {
-    try {
-        // const mongoURI = process.env.MONGODB_URI;
-        const mongoURI = process.env.MONGODB_LOCAL_URI;
-        
-        if (!mongoURI) {
-            
-            return;
-        }
-        
-        await mongoose.connect(mongoURI);
-        
-    } catch (err) {
-        console.error(`Error connecting to the database: ${err}`);
-        
+  try {
+    const mongoURI = process.env.MONGODB_URI;
+    // const mongoURI = process.env.MONGODB_LOCAL_URI;
+
+    if (!mongoURI) {
+      return;
     }
+
+    await mongoose.connect(mongoURI);
+  } catch (err) {
+    console.error(`Error connecting to the database: ${err}`);
+  }
 };
 
 module.exports = connectToDatabase;
